4f26823ca41d90781cc2abbba717caa90c6f5f57
ahmetemincopur <64459050+ahmetemincopur@users.noreply.github.com>
2022-03-10 01:15:37 +0300

    Add files via upload

---
+++ Uygulama5.3/src/EnumUygulama.java
@@ -0,0 +1,20 @@
+//Enum yapilari
+public class EnumUygulama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(IslemlerEnum.Carp.Hesapla(5, 12));
+		//Nesne turetmeksizin yapisal olarak Hesaplama fonksiyonu 
+		//ve diger fonksiyonlarin kullanilmasini saglar
+		
+		System.out.println(IslemlerEnum.Carp.GetA());
+		System.out.println(IslemlerEnum.Bolme.GetA());		
+		//IslemlerEnum.GetA();
+		//Static fonksiyon icin gecerli
+		
+		
+		System.out.println(ParametreliEnum.Gokhan.urlGoster());
+		System.out.println(ParametreliEnum.ISTE.urlGoster());
+	}
+
+}
